be16354cb59c303a57aae085a05d0ebeb073741e
Хлебников Андрей Александрович <Andrei.Khlebnikov@domrf.ru>
2021-05-31 10:52:32 +0300

    Практические: 10

---
+++ regexp/regexp_nums_2.c
@@ -0,0 +1,14 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+/**
+((36[0-8](5[0-3][567]?9?8?12)?46)|(12[67]?92[0-9]10*36?)|((80[67]36?5)?76?5?89(10|11|12)?26))00
+**/
+
+int main(int argc, char **argv) {
+    return EXIT_SUCCESS;
+}
