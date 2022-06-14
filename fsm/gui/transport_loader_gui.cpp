be081fd77fed4b8a10d4299cf5fa3bbeb6dca3ca
Хлебников Андрей Александрович <Andrei.Khlebnikov@domrf.ru>
2022-06-15 00:10:33 +0300

    Tails

--- fsm/gui/transport_loader_gui.cpp
+++ fsm/gui/transport_loader_gui.cpp
@@ -7,17 +7,14 @@
 #include <raylib.h>
 #include <cassert>
 #include <memory>
+#include <utility>
 #include <vector>
+#include <memory>
 
 struct Configuration configuration;
 
 #define TILE_SIZE     16
 #define MARGIN_SIZE   2
-
-#define RESOURCE_GRASS   0
-#define RESOURCE_DIRT    1
-#define RESOURCE_THINGS  2
-#define RESOURCE_COW     3
 
 static int map[] = {
         20, 19, 12, 21, 28, 11, 13, 29, 27, 36,
@@ -49,11 +46,25 @@ static int map[] = {
 };
 
 class Object {
+protected:
     float x;
     float y;
     bool visible;
-private:
-    virtual void update() {
+public:
+    Object() : x(0), y(0), visible(false) {}
+
+    virtual ~Object() = default;
+
+    void location(float cx, float cy) {
+        x = cx;
+        y = cy;
+    }
+
+    void visibility(bool v) {
+        visible = v;
+    }
+
+    virtual void update(int) {
     }
 
     virtual void render() {
@@ -61,18 +72,13 @@ private:
         (void) y;
         (void) visible;
     }
-
-public:
-    Object() : x(0), y(0), visible(false) {}
-
-    virtual ~Object() = default;
 };
 
 class Resource final {
     std::shared_ptr<Rectangle[]> rects;
-    Texture texture{};
-    int rects_length{};
-    int rect_size{};
+    Texture texture;
+    int rects_length;
+    int rect_size;
 
 public:
     void load(const char *filename, int size = TILE_SIZE) {
@@ -115,10 +121,13 @@ public:
     }
 
     Resource() : rects(), texture(), rects_length(0), rect_size(0) {}
+
     explicit Resource(const char *filename) : Resource() {
         load(filename);
     }
+
     Resource(const Resource &) = default;
+
     Resource &operator=(const Resource &that) = default;
 
     virtual ~Resource() {
@@ -126,186 +135,238 @@ public:
     }
 };
 
+class Cow final: public Object {
+    int count;
+public:
+    Cow(const Resource &resource) : resource(resource) {
+    }
+
+    void update(int time) override {
+        if (time % 66 == 0) {
+            ++count;
+            if (count > 1) {
+                count = 0;
+            }
+        }
+    }
+
+    void render() override {
+        if (visible)
+            render(count);
+    }
+
+private:
+    void render(int i) {
+        auto size = resource.size();
+        switch (i) {
+            default:
+            case 0: {
+                resource.draw(0, x, y);
+                resource.draw(4, x + size, y);
+                resource.draw(1, x, y + size);
+                resource.draw(5, x + size, y + size);
+                break;
+            }
+            case 1: {
+                resource.draw(8, x, y);
+                resource.draw(12, x + size, y);
+                resource.draw(9, x, y + size);
+                resource.draw(13, x + size, y + size);
+                break;
+            }
+            case 2: {
+                resource.draw(16, x, y);
+                resource.draw(20, x + size, y);
+                resource.draw(17, x, y + size);
+                resource.draw(21, x + size, y + size);
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+        }
+
+    }
+
+    const Resource &resource;
+};
+
+class Player: public Object {
+    void render() override {
+        struct Rectangle player = {x, y, 32, 64};
+        DrawRectangleLinesEx(player, 2, RED);
+    }
+};
+
 class Game {
-    std::vector<Object> objects;
+    std::vector<Object *> objects;
     std::vector<Resource> resources;
     struct Configuration configuration;
+    int tile_index = 0, resource_index = 0, time = 0;
+    Vector2 mouse;
 public:
     enum ResourceId {
         Grass, Dirt, Things, Cow
     };
 
-    Game(struct Configuration conf): resources(4), configuration(conf) {
-        resources[Grass] = Resource("assets/Tilesets/Grass.png");
-        resources[Dirt] = Resource("assets/Tilesets/Tilled.Dirt.png");
-        resources[Things] = Resource("assets/Objects/Basic.Grass.Biom.Things.1.png");
-        resources[Cow] = Resource("assets/Characters/Free.Cow.Sprites.png");
+    explicit Game(const char *const configuration_file) : resources(4), configuration{} {
+        resources[Grass].load("assets/Tilesets/Grass.png");
+        resources[Dirt].load("assets/Tilesets/Tilled.Dirt.png");
+        resources[Things].load("assets/Objects/Basic.Grass.Biom.Things.1.png");
+        resources[Cow].load("assets/Characters/Free.Cow.Sprites.png");
+        read_configuration(&configuration, configuration_file);
+    }
+
+    Game &add(Object *object) {
+        objects.insert(objects.cbegin(), object);
+        return *this;
+    }
+
+    void cow(float x, float y) {
+        auto cow = new class Cow(resource(Cow));
+        cow->location(x, y);
+        cow->visibility(true);
+        add(cow);
+    }
+
+    void player(float x, float y) {
+        auto player = new class Player;
+        player->location(x, y);
+        player->visibility(true);
+        add(player);
     }
 
     void render() {
         draw_resources();
+        for (auto &object: objects) {
+            object->render();
+        }
+        draw_gud();
     }
 
-    [[nodiscard]] const Resource &resource(ResourceId id) const {
-        return resources[id];
-    }
-private:
-    void draw_resources() {
-//        draw_resources(&configuration.map, Grass, [](size_t x) { return (size_t)map[x]; });
-        draw_resources(&configuration.things, Things);
-        draw_resources(&configuration.paths, Dirt);
-    }
-    void draw_resources(struct MapMatrix *mat, const ResourceId id, size_t (*transform)(size_t) = [](size_t x) { return x; }) {
-        const auto &res = resource(id);
-        for (float row = 0; row < mat->size; ++row) {
-            for (float col = 0; col < mat->size; ++col) {
-                auto idx = (size_t) matrix_get(mat, col, row);
-                if (idx == 0)
-                    continue;
-                idx = (*transform)(idx);
-                res.draw(idx - 1, row * res.size(), col * res.size());
-            }
-        }
-    }
-};
-
-static void write_cow(const Resource &cow, float x, float y, int i) {
-    auto size = cow.size();
-    switch (i) {
-        default:
-        case 0: {
-            cow.draw(0, x, y);
-            cow.draw(4, x + size, y);
-            cow.draw(1, x, y + size);
-            cow.draw(5, x + size, y + size);
-            break;
-        }
-        case 1: {
-            cow.draw(8, x, y);
-            cow.draw(12, x + size, y);
-            cow.draw(9, x, y + size);
-            cow.draw(13, x + size, y + size);
-            break;
-        }
-        case 2: {
-            cow.draw(16, x, y);
-            cow.draw(20, x + size, y);
-            cow.draw(17, x, y + size);
-            cow.draw(21, x + size, y + size);
-            break;
-        }
-        case 3: {
-            break;
-        }
-        case 4: {
-            break;
-        }
-    }
-
-}
-
-int
-main(int argc, char **argv) {
-    Resource grass, dirt, things, cow;
-    Resource *resources[] = {&grass, &dirt, &things, &cow}, *selected;
-    struct Rectangle player = {55, 395, 32, 64};
-    size_t row, col;
-    int tile_index = 0, resource_index = 0, resources_size =
-            sizeof(resources) / sizeof(resources[0]), cow_count = 0, time = 0;
-    Vector2 mouse;
-
-    (void) argc;
-    (void) argv;
-    read_configuration(&configuration, "factory.json");
-//    Game g(configuration);
-    InitWindow((int) configuration.map.size * TILE_SIZE + MARGIN_SIZE,
-               (int) configuration.map.size * TILE_SIZE + MARGIN_SIZE, "Transport");
-
-    grass.load("assets/Tilesets/Grass.png");
-    dirt.load("assets/Tilesets/Tilled.Dirt.png");
-    things.load("assets/Objects/Basic.Grass.Biom.Things.1.png");
-    cow.load("assets/Characters/Free.Cow.Sprites.png");
-
-    SetTargetFPS(60);
-    ShowCursor();
-    while (!WindowShouldClose()) {
+    void update() {
+        time = GetTime() * 100;
         mouse = GetMousePosition();
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
+
+        for (auto object: objects) {
+            object->update(time);
+        }
 
         if (IsKeyPressed(KEY_LEFT)) {
             resource_index++;
-            if (resource_index >= resources_size) {
+            if (resource_index >= (int) resources.size()) {
                 resource_index = 0;
             }
         } else if (IsKeyPressed(KEY_RIGHT)) {
             resource_index--;
             if (resource_index < 0) {
-                resource_index = resources_size - 1;
+                resource_index = (int) resources.size() - 1;
             }
         }
-        selected = resources[resource_index];
+        if (resource_index >= (int) resources.size())
+            return;
 
+        const Resource &at = resources.at(resource_index);
         if (IsKeyPressed(KEY_UP)) {
             tile_index++;
-            if (tile_index >= selected->count())
+            if (tile_index >= at.count())
                 tile_index = 0;
         } else if (IsKeyPressed(KEY_DOWN)) {
             tile_index--;
             if (tile_index < 0) {
-                tile_index = selected->count() - 1;
+                tile_index = at.count() - 1;
             }
         }
+    }
 
-        for (row = 0; row < configuration.map.size; ++row) {
-            for (col = 0; col < configuration.map.size; ++col) {
-                Resource *c;
-                auto idx = (size_t) matrix_get(&configuration.map, col, row);
-                int real;
+    [[nodiscard]] const Resource &resource(ResourceId id) const {
+        return resources[id];
+    }
 
-                real = map[idx];
-                if (idx > 50 && idx < 100) {
-                    c = resources[RESOURCE_DIRT];
-                } else if (idx >= 100 && idx < 150) {
-                    c = resources[RESOURCE_THINGS];
-                } else if (idx >= 150) {
-                    c = resources[RESOURCE_COW];
-                } else {
-                    c = resources[RESOURCE_GRASS];
-                }
-
-                if (real == 0) {
-                    c = selected;
-                    real = tile_index;
-                }
-
-                c->draw(real, row * c->size(), col * c->size());
-            }
-        }
-//        g.render();
-
-        time = GetTime() * 100;
-        if (time % 66 == 0) {
-            ++cow_count;
-            if (cow_count > 1) {
-                cow_count = 0;
-            }
-        }
-        write_cow(cow, 50, 50, cow_count);
-        write_cow(cow, 200, 300, cow_count);
-        write_cow(cow, 430, 500, cow_count);
-
+private:
+    void draw_gud() const {
         DrawText(TextFormat("FPS: %i", GetFPS()), 2 + MARGIN_SIZE, 2 + MARGIN_SIZE, 16, GRAY);
         DrawText(TextFormat("TLE: %i", tile_index), 70 + MARGIN_SIZE, 2 + MARGIN_SIZE, 16, GRAY);
         DrawText(TextFormat("RSC: %i", resource_index), 130 + MARGIN_SIZE, 2 + MARGIN_SIZE, 16, GRAY);
         DrawText(TextFormat("TIM: %i", time), 190 + MARGIN_SIZE, 2 + MARGIN_SIZE, 16, GRAY);
         DrawText(TextFormat("MSE: (%03i, %03i)", (int) mouse.x, (int) mouse.y), 410 + MARGIN_SIZE, 2 + MARGIN_SIZE, 16,
                  GRAY);
+    }
 
-        DrawRectangleLinesEx(player, 2, RED);
+    void draw_resources() {
+        for (size_t row = 0; row < configuration.map.size; ++row) {
+            for (size_t col = 0; col < configuration.map.size; ++col) {
+                Resource &c = resources[Grass];
+                auto idx = (size_t) matrix_get(&configuration.map, col, row);
+                int real;
 
+                real = map[idx];
+                if (idx > 50 && idx < 100) {
+                    c = resources[Dirt];
+                } else if (idx >= 100 && idx < 150) {
+                    c = resources[Things];
+                } else if (idx >= 150) {
+                    c = resources[Cow];
+                } else {
+                    c = resources[Grass];
+                }
+
+                if (real == 0) {
+                    c = resource(static_cast<ResourceId>(resource_index));
+                    real = tile_index;
+                }
+
+                c.draw(real, row * c.size(), col * c.size());
+            }
+        }
+        draw_resources(&configuration.things, Things);
+        draw_resources(&configuration.paths, Dirt);
+    }
+
+    void draw_resources(struct MapMatrix *mat, const ResourceId id) {
+        const auto &res = resource(id);
+        for (size_t row = 0; row < mat->size; ++row) {
+            for (size_t col = 0; col < mat->size; ++col) {
+                auto idx = (size_t) matrix_get(mat, col, row);
+                if (idx == 0) {
+                    continue;
+                }
+                res.draw(idx - 1, row * res.size(), col * res.size());
+            }
+        }
+    }
+
+public:
+    virtual ~Game() {
+        for (auto it = objects.begin(); it != objects.end(); ++it) {
+            delete *it;
+            objects.erase(it);
+        }
+    }
+};
+
+int
+main(int argc, char **argv) {
+    (void) argc;
+    (void) argv;
+    InitWindow((int) 34 * TILE_SIZE + MARGIN_SIZE,
+               (int) 34 * TILE_SIZE + MARGIN_SIZE, "Transport");
+    SetTargetFPS(60);
+    ShowCursor();
+    Game g("factory.json");
+    g.cow(50, 50);
+    g.cow(200, 300);
+    g.cow(430, 500);
+    g.player(55, 395);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        g.update();
+        g.render();
         EndDrawing();
-
         if (IsKeyPressed(KEY_C)) {
             Image screen = LoadImageFromScreen();
             ExportImage(screen, "export.png");
